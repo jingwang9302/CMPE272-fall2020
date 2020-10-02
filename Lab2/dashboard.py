71cfd48601274a329cbc5141930366b53584f239
JingWang <jingwang9302@gmail.com>
2020-10-02 16:12:44 -0700

    Change output to output.txt file

--- Lab2/dashboard.py
+++ Lab2/dashboard.py
@@ -1,13 +1,16 @@
 import time
 import zmq
+import json
 
 
 def result_collector():
     context = zmq.Context()
     results_receiver = context.socket(zmq.PULL)
     results_receiver.bind("tcp://127.0.0.1:5557")
+    outfile = open("output.txt", "w")
     for x in range(10):
         result = results_receiver.recv_json()
+        json.dump(result, outfile)
         print(result)
 
 
